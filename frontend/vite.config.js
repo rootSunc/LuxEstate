c4b71b7add5a10a7b63a20974814e1ee4662de59
rootSunc <sunchao686@gmail.com>
2024-03-29 23:52:36 +0800

    complete signup page functionality, but the error handling is still not the best practice

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",  // 所有对'/api'路径的请求都会代理到'localhost:3000'服务器
+        secure: false, // proxy不会验证SSL证书
+      },
+    },
+  },
   plugins: [react()],
-})
+});
