2e5a6ea87a05fa25e58d3ebcfb1b86d1e03471be
rootSunc <sunchao686@gmail.com>
2024-04-15 00:32:01 +0800

    complete show all listing of user and finish delete listing function

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { deleteUser, updateUser, userController } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  updateUser,
+  userController,
+  getUserListings,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,4 +12,5 @@ const router = express.Router();
 router.get("/test", userController);
 router.post("/update/:id", verifyToken, updateUser); // 路由定义中连续使用多个函数时，这些函数将按照他们列出的顺序执行，前一个函数通过调用`next()`将req传递给下一个函数
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 export default router;
