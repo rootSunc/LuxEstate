1d51fd653efa57e0bd96a529ba33b19fd220c5b0
rootSunc <sunchao686@gmail.com>
2024-04-17 14:55:29 +0800

    complete show all listing of user and finish delete listing function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,8 @@ export default function App() {
           <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/about" element={<About />} />
+          {/* <Route path="/listing/:listingId" element={<Listing />} /> */}
+
           <Route element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
             <Route path="/create-listing" element={<CreateListing />} />
