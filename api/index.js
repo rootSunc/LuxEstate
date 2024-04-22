7da7a4592a0e192720d2aee7012dc07c5b56ce1d
rootSunc <sunchao686@gmail.com>
2024-04-22 18:00:27 +0800

    adjast vercel config

--- api/index.js
+++ api/index.js
@@ -22,9 +22,12 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server is runing on port 3000");
-});
+if (!process.env.VERCEL) {
+  // 仅在非 Vercel 环境中添加监听
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+}
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
