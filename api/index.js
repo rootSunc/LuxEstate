a28cfc1d6bfa908bbea5b8eb2bfcc1a5acf49c9f
rootSunc <sunchao686@gmail.com>
2024-05-01 14:36:27 +0800

    fix frontend fold name

--- api/index.js
+++ api/index.js
@@ -30,9 +30,9 @@ app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 // error middleware
